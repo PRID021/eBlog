af18801afefdb4add35c1718f1dfdbc6d579568a
hoang.pham <hoang.pham@executionlab.asia>
2024-12-18 23:56:58 +0700

    Initial Commit

---
+++ eBlog/eBlogApp.swift
@@ -0,0 +1,17 @@
+//
+//  eBlogApp.swift
+//  eBlog
+//
+//  Created by mac on 18/12/24.
+//
+
+import SwiftUI
+
+@main
+struct eBlogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ eBlogTests/eBlogTests.swift
@@ -0,0 +1,17 @@
+//
+//  eBlogTests.swift
+//  eBlogTests
+//
+//  Created by mac on 18/12/24.
+//
+
+import Testing
+@testable import eBlog
+
+struct eBlogTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
